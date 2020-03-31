8958efbf52ade1c303e081a7d3635f83ce18b68b
samzhang <samzhangqingchuan@icloud.com>
2020-04-01 00:55:51 +1300

    remove old version

--- Educational DP Contest Editorial/Code/D.cpp
+++
@@ -1,9 +0,0 @@
-//注意要开 long long
-for(int i = 1;i<=n;i++)
-    for(int j = 0;j<=W;j++)
-        if(j < w[i])
-        //容量不够
-            dp[i][j] = dp[i-1][j];
-        else
-        //容量足够
-            dp[i][j] = max(dp[i-1][j-w[i]] + v[i], dp[i-1][j]);
--- Educational DP Contest Editorial/Code/H.cpp
+++
@@ -1,8 +0,0 @@
-dp[1][1] = 1;
-for(int i = 1; i <= h; i++) {
-    for(int j = 1; j <= w; j++) {
-        if(i == 1 and j == 1)continue;
-        if(dp[i][j] == '.') //如果可以经过
-            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-    }
-}
