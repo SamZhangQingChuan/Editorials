4fdbfe4a19536f01f4348d67f42db33a5c702cc3
samzhang <samzhangqingchuan@icloud.com>
2020-04-04 05:18:20 +1300

    change to symlink

--- Atcoder Educational DP Contest Editorial/源代码/Code/M.cpp
+++
@@ -1,9 +0,0 @@
-dp[0][0] = 1;
-for(int i = 1; i <= n; i++) {
-    dp[i][0] = 1;
-    for(int j = 0; j < k; j++) {
-        dp[i][j + 1] = dp[i][j] + dp[i - 1][j + 1];
-        if(j - a[i] >= 0)
-            dp[i][j + 1] -= dp[i - 1][j - a[i]];
-    }
-}
