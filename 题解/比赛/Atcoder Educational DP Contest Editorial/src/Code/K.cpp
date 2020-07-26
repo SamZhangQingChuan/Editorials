0e3336b44b67b1947624493e31faab8826923608
samzhang <samzhangqingchuan@icloud.com>
2020-07-26 22:42:45 +0800

    Update K.cpp

--- 题解/比赛/Atcoder Educational DP Contest Editorial/src/Code/K.cpp
+++ 题解/比赛/Atcoder Educational DP Contest Editorial/src/Code/K.cpp
@@ -4,7 +4,7 @@ for(int i = 1;i<=k;i++){
     dp[i] = false;
     for(int j = 0;j<n;j++){
         //如果存在方法使得对手必败，那么当前必胜
-        if(i >= a[j] and dp[i-a[j] == false){
+        if(i >= a[j] and dp[i-a[j]] == false){
             dp[i] = true;
         }
     }
